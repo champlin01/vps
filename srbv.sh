90373dbf1d61801b8f01cbf89a11ea750d6a48d7
champlin01 <77158735+champlin01@users.noreply.github.com>
2021-02-04 22:48:07 +0700

    Update srbv.sh

--- srbv.sh
+++ srbv.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-START='date +%s'
-while [ $(( $(date +%s) - 60 )) -lt $START ]; do
-  ./srb --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1
+while [ 1 ]
+do
+    ./srb --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1
 done
