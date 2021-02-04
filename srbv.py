e14533cd7e2ddbe2bab8c3e7df37cef1f7711e61
champlin01 <77158735+champlin01@users.noreply.github.com>
2021-02-04 20:59:13 +0700

    Update srbv.py

--- srbv.py
+++ srbv.py
@@ -4,10 +4,13 @@ from time import sleep
 waktu = 1
 
 while True:
+	try:
 	waktu =+ 1
-	if waktu == 5:
-		os.system("./srb --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1")
-		waktu = 1
-	else:
-		print("Wait ...")
-	sleep(1)
+		if waktu == 5:
+			os.system("./srb --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1")
+			waktu = 1
+		else:
+			print("Wait ...")
+		sleep(1)
+	except Exception as e:
+		print(e)
