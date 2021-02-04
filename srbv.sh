f35a0613d9c75e68bad58b8d5f3b387fec56105d
champlin01 <77158735+champlin01@users.noreply.github.com>
2021-02-04 20:03:05 +0700

    Update srbv.sh

--- srbv.sh
+++ srbv.sh
@@ -1,4 +1,4 @@
 while [ 1 ]; do
-timeout -k 5 ./srb --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1
+timeout 5 ./srb --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1
 sleep 3
 done
