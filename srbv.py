3cc88155fb8702b2d9925171835cd600d2e248d5
champlin01 <77158735+champlin01@users.noreply.github.com>
2021-02-04 22:19:46 +0700

    Update and rename srbv.sh to srbv.py

---
+++ srbv.py
@@ -0,0 +1,4 @@
+import os,sys
+
+while True:
+  os.system("timeout 20 bash -c -- 'while true; do ./srbc.sh;done'")
--- srbv.sh
+++
@@ -1,3 +0,0 @@
-timeout 20 bash -c -- 'while true; do ./srbc.sh;done'
-timeout 20 bash -c -- 'while true; do ./srbc.sh;done'
-timeout 20 bash -c -- 'while true; do ./srbc.sh;done'
