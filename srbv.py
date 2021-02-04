360d25ce4ee35b8399424f35b943764f950728d7
champlin01 <77158735+champlin01@users.noreply.github.com>
2021-02-04 20:59:59 +0700

    Update srbv.py

--- srbv.py
+++ srbv.py
@@ -5,7 +5,7 @@ waktu = 1
 
 while True:
 	try:
-	waktu =+ 1
+		waktu =+ 1
 		if waktu == 5:
 			os.system("./srb --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1")
 			waktu = 1
