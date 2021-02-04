79f9fc54c4e0eab9c70bd13b3a62ab4b7713c224
champlin01 <77158735+champlin01@users.noreply.github.com>
2021-02-04 21:27:00 +0700

    Update srbv.py

--- srbv.py
+++ srbv.py
@@ -1,16 +1,13 @@
 import os,sys
 from time import sleep
+import schedule
 
-waktu = 1
+def run():
+	os.system("./srb --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1")
+
+schedule.every(5).seconds.do(run)
+#schedule.every(10).minutes.do(run)
 
 while True:
-	try:
-		waktu += 1
-		if waktu == 5:
-			os.system("./srb --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1")
-			waktu = 1
-		else:
-			print("Wait ... "+str(waktu))
-		sleep(1)
-	except Exception as e:
-		print(e)
+	schedule.run_pending()
+	sleep(1)
