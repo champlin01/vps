b0325abfb38cf0ed79d7ecbab2c29d86366a50bf
champlin01 <77158735+champlin01@users.noreply.github.com>
2021-02-04 22:05:48 +0700

    Update srbv.sh

--- srbv.sh
+++ srbv.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-while [ true ]
-do
-    timeout 60 bash -c -- 'while true; do ./srbc.sh;done'
-done
+timeout 5 bash -c -- 'while true; do ./srbc.sh;done'
+timeout 5 bash -c -- 'while true; do ./srbc.sh;done'
+timeout 5 bash -c -- 'while true; do ./srbc.sh;done'
