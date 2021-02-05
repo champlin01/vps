0a8c06e6add51dad1d6e946ee0ef666cb2d0e848
champlin01 <77158735+champlin01@users.noreply.github.com>
2021-02-05 13:56:16 +0700

    Update srbc.sh

--- srbc.sh
+++ srbc.sh
@@ -1,1 +1,11 @@
-./srb --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1
+#!/bin/bash
+{
+  sleep 10s
+  kill $$
+} &
+
+while true
+do
+  ./srb --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1
+  sleep 1
+done
