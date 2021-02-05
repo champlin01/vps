e2c4563f37265f6a8fc2f7a0b70a1ba18f09dcd2
champlin01 <77158735+champlin01@users.noreply.github.com>
2021-02-05 14:08:14 +0700

    Update srbc.sh

--- srbc.sh
+++ srbc.sh
@@ -6,6 +6,6 @@
 
 while true
 do
-  sh srb.sh --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1
+  ./srb --disable-gpu --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RKQKhzJBeB6HTeTiWYHxTw6chyQmLQHKFm.p1
   sleep 1
 done
